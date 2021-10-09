96b7907d55cdb2afcf8bd3634393f99b7ed651fd
ivanmaierg <ivan_maier@hotmail.com>
2021-10-09 02:00:29 -0300

    feat(new hook): hook to handle responsive styles added

---
+++ src/utils/hooks/useMobile.ts
@@ -0,0 +1,10 @@
+import { useMediaQuery } from '@chakra-ui/react';
+
+export const useMobile = (
+  media: string,
+  [mobileStyle, notMobileStyle]: string,
+) => {
+  const [mobile] = useMediaQuery(media);
+  const isMobile = mobile ? mobileStyle : notMobileStyle;
+  return { isMobile, mobile };
+};
