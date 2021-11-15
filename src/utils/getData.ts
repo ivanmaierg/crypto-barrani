6822a599ec1aa80cc3c4eef52322db5dc1ea4356
ivanmaierg <ivan_maier@hotmail.com>
2021-11-14 21:13:04 -0300

    feat(getdata): add getCoins fetcher

--- src/utils/getData.ts
+++ src/utils/getData.ts
@@ -35,3 +35,15 @@ export async function getHistory(url: string): Promise<HistoryCurrencyData[]> {
   }
   return latest.json();
 }
+
+export async function getCoins(): Promise<any> {
+  const latest = await fetch(`api/crypto/coins`);
+  if (!latest.ok) {
+    const error: any = new Error(`An error occurred while fetching the data.`);
+    // Attach extra info to the error object.
+    error.info = await latest.json();
+    error.status = latest.status;
+    throw error;
+  }
+  return latest.json();
+}
