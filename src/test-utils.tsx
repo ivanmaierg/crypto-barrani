ccccd29822885c63f0cbc4aaf3ac907c1b321c7a
ivanmaierg <ivan_maier@hotmail.com>
2022-01-18 18:08:55 -0300

    fix(test-utils): fix path of ThemeProvider

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -1,8 +1,8 @@
 import React, { FC, ReactElement } from 'react';
 import { render, RenderOptions } from '@testing-library/react';
 
-import { ThemeProvider } from '@chakra-ui/skeleton/node_modules/@chakra-ui/system';
 import { myTheme } from '@/theme/theme';
+import { ThemeProvider } from '@chakra-ui/react';
 
 const AllTheProviders: FC = ({ children }) => (
   <ThemeProvider theme={myTheme}>{children}</ThemeProvider>
